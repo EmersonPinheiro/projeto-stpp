0d2428f3b4ced95b41b1ea8a96f03fda24c345c6
Emerson Pinheiro <e_pms@hotmail.com>
2017-09-12 13:34:39 +0000

    Download material

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -13,6 +13,8 @@ class MaterialController extends Controller
       $url = DB::table('Material')->where('cod_material', $id)->select('Material.url_documento')->first();
       $pathToFile = storage_path()."/app/".$url->url_documento;
       return response()->download($pathToFile, 'documento.pdf');
+
+      //FAZER O DOWNLOAD DO ZIP DAS IMAGENS
     }
 
     public function showMaterial($id)
